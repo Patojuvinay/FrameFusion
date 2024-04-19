558e4e506b33679e7b6d0d165eb624af78e12441
Patojuvinay <167427153+Patojuvinay@users.noreply.github.com>
2024-04-19 22:18:32 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    $("#num1").countMe(40,50);
+    $("#num2").countMe(20, 8);
+    $("#num3").countMe(2, 5);
+});
+
+
+
+
+$(function() {
+    $('.gallery a').lightbox(); 
+  });
